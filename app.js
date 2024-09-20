e3a3da7e2f3454bbbd41f947770bcf8a21770df2
vincentL233 <vincent02282010@gmail.com>
2024-09-20 15:33:02 +0800

    int

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const app = express();
 const port = 4000;
 
 // 在伺服器啟動時執行
-var weight = 50;
-var height = 150;
-var bmi = weight / ((height / 100) ** 2);
-console.log("Your BMI is " + bmi);
-
+function calcBmi(){
+  var weight = parseInt(document.gerElemenById("weight"));
+  var height = parseInt(document.gerElemenById("height"));
+  var bmi = weight / ((height / 100) ** 2);
+  console.log("Your BMI is " + bmi);
+}
 // 設置根路徑的 GET 請求
 app.get('/', (req, res) => {
   res.send(`Hello, welcome to your Node.js server! Your BMI is ${bmi.toFixed(2)}`);
