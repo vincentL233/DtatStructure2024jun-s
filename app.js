f21245e0820bbacb96293dd47583ccf910ce6a47
vincentL233 <vincent02282010@gmail.com>
2024-09-20 15:15:15 +0800

    setbmi

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-var weight =50
-var height =150
-var bmi =weight/((height/100)**2);
-console.log("your bmi is"+bmi)
-// 引入 Express
 const express = require('express');
 const app = express();
-const port = 4000; // 伺服器運行的端口
+const port = 4000;
+
+// 在伺服器啟動時執行
+var weight = 50;
+var height = 150;
+var bmi = weight / ((height / 100) ** 2);
+console.log("Your BMI is " + bmi);
 
 // 設置根路徑的 GET 請求
 app.get('/', (req, res) => {
-  res.send('Hello, welcome to your Node.js server!');
+  res.send(`Hello, welcome to your Node.js server! Your BMI is ${bmi.toFixed(2)}`);
 });
 
-// 啟動伺服器，並監聽指定的端口
+// 啟動伺服器
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
