ffbf5c875218a9dd46900f60871ce6308b8fc5fe
vincentL233 <vincent02282010@gmail.com>
2024-09-27 16:03:04 +0800

    swtich

--- nodebmi.js
+++ nodebmi.js
@@ -1,7 +1,23 @@
 var readline = require("readline-sync");
 
-var weight = readline.question("Please enter your weight(kg)?");
-var height =readline.question("Please enter your height(cm)?");
+var weight = readline.questionInt("Please enter your weight(kg)?");
+var height =readline.questionInt("Please enter your height(cm)?");
 var bmi =weight/((height/100)**2);
+var category;
 
-console.log("your bmi is" + bmi);
+switch (true) {
+    case (bmi >= 27):
+        category = "obese";
+        break;
+    case (bmi >= 24):
+            category = "overweight";
+        break;   
+    case (bmi >= 18.5):
+        category = "underweight";
+        break;
+    default:
+        category = "too light";
+        break;
+}
+
+console.log("Your BMI is " + bmi + ", you are " + category + ".");
