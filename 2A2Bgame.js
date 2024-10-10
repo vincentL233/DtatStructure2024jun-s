41641d78eeebcfd6f29450c3d597a771805b51a6
vincentL233 <vincent02282010@gmail.com>
2024-10-10 21:47:41 +0800

    2A2Bdone

--- 2A2Bgame.js
+++ 2A2Bgame.js
@@ -7,12 +7,37 @@ for(var i = 0; i<4; i++){
     ans[i] = ans[random_number];
     ans[random_number]=temp;
 };
+var secret = ans.slice(0,4).join("");
+
+function getAB(secret,guess){
+    var A = 0;
+    var B = 0;
+    for(var i = 0; i<4;i++){
+        if(secret[i] === guess[i]){
+            A++;
+        } else if (secret.includes(guess[i])){
+            B++;
+        }
+    }
+    return{A,B};
+}
+
+var attempts = 0;
+var G;
+
 do{
-    var G = readline.questionInt("Please input 4 disgits? ")
-}while(Ｇ<1000 || G> 10000);
-var gstr = G.toString();
-for(){
-    for(){
-        
+     G = readline.question("請輸入一個 4 位數的數字: ")
+    if(G.length !== 4 || isNaN(G)){
+        console.log("無效的輸入。請輸入正好 4 位數。");
+        continue;
+    }
+    attempts++;
+    var {A,B} = getAB(secret,G);
+    console.log(A + "A" + B + "B");
+    if(A === 4){
+        console.log("恭喜你！正確答案是" + secret +",你總共猜了"+ attempts +"次。");
+        break;
     }
 }
+
+while(true);
